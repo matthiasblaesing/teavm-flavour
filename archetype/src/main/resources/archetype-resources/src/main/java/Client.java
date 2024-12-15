961cbcc267854f3f7263f959aa00f115a44fe20b
ScraM Team <bmp-githscr@frequal.com>
2024-12-15 17:17:49 +0100

    In archetype, fixed hiding static message at startup

--- archetype/src/main/resources/archetype-resources/src/main/java/Client.java
+++ archetype/src/main/resources/archetype-resources/src/main/java/Client.java
@@ -2,21 +2,23 @@ package ${package};
 
 import org.teavm.flavour.templates.BindTemplate;
 import org.teavm.flavour.widgets.ApplicationTemplate;
+import org.teavm.jso.browser.Window;
 
 @BindTemplate("templates/client.html")
 public class Client extends ApplicationTemplate {
-    private String userName = "";
+  private String userName = "";
 
-    public static void main(String[] args) {
-        Client client = new Client();
-        client.bind("application-content");
-    }
+  public static void main(String[] args) {
+    Window.current().getDocument().getElementById("application-content").clear();
+    Client client = new Client();
+    client.bind("application-content");
+  }
 
-    public String getUserName() {
-        return userName;
-    }
+  public String getUserName() {
+    return userName;
+  }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
 }
